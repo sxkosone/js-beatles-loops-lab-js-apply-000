4ccd1b3425b2172bdf4a2ea1e62de88e2f1f8b23
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-23 14:09:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function johnLennonFacts(facts) {
   }
   return facts;
 }
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return array;
+}
